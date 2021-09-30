6213abc95e1d2fa6869ee62c938a854f34ac3dab
Aleksey Polchikov <keksley@gmail.com>
2021-09-30 17:46:54 +0500

    Фикс типа контента

--- script.js
+++ script.js
@@ -151,9 +151,9 @@ function _postData() {
             return fetch(url, {
               method: 'POST',
               // *GET, POST, PUT, DELETE, etc.
-              // headers: {
-              //   'Content-Type': 'application/json',
-              // },
+              headers: {
+                'Content-Type': 'application/json',
+              },
               body: JSON.stringify(data) // body data type must match "Content-Type" header
 
             });
--- src/script.js
+++ src/script.js
@@ -93,9 +93,9 @@ function getHeaderElement(eventStartDateTime, id) {
 async function postData(url = '', data = {}) {
   const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // },
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
   return await response.json(); // parses JSON response into native JavaScript objects
